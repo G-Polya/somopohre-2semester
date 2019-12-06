0e765e0d7a89d1ffefdcfa87f5e6e57969117fe6
G_Polya <wlskaka4@gmail.com>
2019-12-06 18:26:15 +0900

    one,two_line

--- 시소프/13주차/week13/week13/소스.cpp
+++ 시소프/13주차/week13/week13/소스.cpp
@@ -14,8 +14,7 @@ char* SubString(char* input, int begin, int end)
 	substring[end - begin + 1] = 0;
 	return substring;
 }
-
-
+void address_and_instruction(int address, char* input);
 
 void get_capacity(char* input)
 {
@@ -38,8 +37,8 @@ int main(int argc, char* argv[])
 		fgets(buffer, sizeof(buffer), objfile);
 		if (buffer[0] == 'T')
 		{
-			char* temp_length = SubString(buffer, 7, 8);
-			count += strtol(temp_length, NULL, 16);
+			char* line_length = SubString(buffer, 7, 8);
+			count += strtol(line_length, NULL, 16);
 		}
 		
 		len = strlen(buffer);
@@ -61,17 +60,59 @@ int main(int argc, char* argv[])
 	int end_addr = start_addr + count - 1;
 	printf("memory last address : %d\n", end_addr);
 
-	printf("%s\n", fgets(buffer, sizeof(buffer), objfile));
-	printf("%s\n", fgets(buffer, sizeof(buffer), objfile));
+	//printf("%s", fgets(buffer, sizeof(buffer), objfile));
+	//printf("%s", fgets(buffer, sizeof(buffer), objfile));
 
 	printf("%d\n", count);
 
 	// address  instruction
 	int address = start_addr; // 초기화
 
+	printf("---------one line--------\n");
+	char* one_line = fgets(buffer, sizeof(buffer), objfile);
+	printf("%s", one_line);
+	int one_line_length = strlen(one_line) - 2; // 문자열의 마지막 널문자 제거(-1), 공백문자 제거(-1)
+	
 
+	char* temp = SubString(one_line, 9, one_line_length);
+	
+	int temp_length = strlen(temp) -1;
+	printf("%s\n\n", temp);
+	
+	for (int i = 0; i < temp_length; i *= 2)
+	{
+		printf("%d %s\n", address, SubString(temp, 0, 1));
 
+		temp = SubString(temp, 2, temp_length);
+		temp_length = strlen(temp) - 1;
+		address += 1;
+	}
+	
+	printf("---------two line--------\n");
+	char* two_line = fgets(buffer, sizeof(buffer), objfile);
+	printf("%s", two_line);
+	int two_line_length = strlen(two_line) - 2;
+	temp = SubString(two_line, 9, two_line_length);
+	temp_length = strlen(temp) - 1;
+	printf("%s\n\n", temp);
+
+	for (int i = 0; i < temp_length; i *= 2)
+	{
+		printf("%d %s\n", address, SubString(temp, 0, 1));
+
+		temp = SubString(temp, 2, temp_length);
+		temp_length = strlen(temp) - 1;
+		address += 1;
+	}
 
 
 	return 0;
+}
+
+void address_and_instruction(int address, char* input)
+{
+	int temp_length = strlen(input) - 1;
+	char* temp = SubString(input, 2, temp_length);
+
+	printf("%d %s\n", address, SubString(input, 0, 1));
 }
