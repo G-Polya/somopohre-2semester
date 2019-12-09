323088808bb727c398430de62ca1c0adad4d6eb5
Acbik <Greedy@msi>
2019-12-09 14:26:52 +0900

    끝낫다...

--- 시소프/week13/week13/main.c
+++ 시소프/week13/week13/main.c
@@ -7,6 +7,58 @@
 
 #define  random_range (32758);
 
+
+
+int get_Upcasting(long x)
+{
+	//x가 16진수로 1자리면 +16(16진수 10)	후 제일 첫번째 자리
+	//x가 16진수로 2자리(10~99) +256(16진수 100)
+	//x가 16진수로 3자리(100~999) +4096(16진수 1000)
+
+
+	if (x < 16)
+		return x / 16;
+	else if (x >= 16 && x < 256)		//10~99 
+		return x / 16;
+	else if (x >= 256 && x < 4096)		//100~999
+		return x / 256;
+	else if (x >= 4096 && x < 65536)	//1000~9999
+		return (x + 4096) / 4096;
+
+}
+
+long get_remainer(int x)
+{
+
+
+	if (x >= 16 && x < 256)
+		return get_Upcasting(x) * 16 - x;
+	else if (x >= 256 && x < 4096)
+		return get_Upcasting(x) * 256 - x;
+	else if (x >= 4096 && x < 65536)
+		return get_Upcasting(x) * 4096 - x;
+
+
+}
+
+int loop(int k, int instruction, int start_addr, int location_count, int i, int* location, int limit)
+{
+	if (instruction + start_addr < limit * k)
+	{
+		for (int j = 0; j < location_count; j++)
+		{
+			if (i - 1 == location[j])
+			{
+				return 1;
+			}
+			else if (i - 2 == location[j])
+			{
+				return (instruction + start_addr - limit * i);
+			}
+		}
+	}
+}
+
 char* SubString(char* input, int begin, int end)
 {
 	char* substring = (char*)malloc(sizeof(char) * (end - begin + 2));
@@ -15,10 +67,6 @@ char* SubString(char* input, int begin, int end)
 	return substring;
 }
 
-//void get_capacity(char* input)
-//{
-//	
-//}
 
 int main(int argc, char* argv[])
 {
@@ -32,7 +80,7 @@ int main(int argc, char* argv[])
 	int* locations = (int*)malloc(sizeof(int) * location_count);
 	//FILE* objfile = fopen(argv[1], "r");
 	FILE* objfile = fopen("source.obj", "r");
-	
+
 	while (!feof(objfile))
 	{
 
@@ -46,16 +94,16 @@ int main(int argc, char* argv[])
 		else if (buffer[0] == 'M')
 		{
 			char* location = SubString(buffer, 1, 6); // relocation이 일어나야하는 위치
-			locations[location_count] = strtol(location,NULL,16);
-			
+			locations[location_count] = strtol(location, NULL, 16);
+
 			location_count++;
 		}
-		
+
 		len = strlen(buffer);
 
 		if (len == 0)
 			break;
-		
+
 		buffer[0] = '\0';
 	}
 
@@ -65,22 +113,22 @@ int main(int argc, char* argv[])
 
 	printf("---------------------Result-------------------\n");
 
-	printf("memory storage capacity: %X\n", strtol(SubString(buffer, 15, 19),NULL,16));
+	printf("memory storage capacity: %X\n", strtol(SubString(buffer, 15, 19), NULL, 16));
 
-	int start_addr = (int)rand() % random_range;
-	//start_addr = 2260;
-	
+	long start_addr = (int)rand() % random_range;
+
+
 	printf("memory start address : %d\n", start_addr);
 	int end_addr = start_addr + count - 1;
 	printf("memory last address : %d\n", end_addr);
 
-	
+
 
 	// address  instruction
 	int address = start_addr; // 초기화
 
-	
-	int all_length = 0;	
+
+	int all_length = 0;
 
 	char** lines = (char**)malloc(sizeof(char*) * T_count);
 	char** temps = (char**)malloc(sizeof(char*) * T_count);
@@ -95,10 +143,9 @@ int main(int argc, char* argv[])
 		all_length += temps_length[i];
 
 	}
-	
-	all_length -= 1;
-	
-	
+
+
+
 	char* all_instruction = (char*)malloc(sizeof(char));	//T로 시작하는 목적코드들에서 시작주소와 길이를 제외한 "실질적인 코드"부분을 하나의 문자열로 합침. 그 하나의 문자열 
 	all_instruction = SubString(all_instruction, 5, all_length);
 	all_instruction[0] = 0;
@@ -107,59 +154,70 @@ int main(int argc, char* argv[])
 		strcat(all_instruction, temps[i]);
 	}
 
-	
+
 	char* temp_all = SubString(all_instruction, 0, all_length);
-	
-	
+
 	//FILE* after_relocation = fopen(argv[2], "w");
 	FILE* after_relocation = fopen("after.txt", "w");
 
-	
-	
+	int relocation_addr_int[3];
+	char relocation_addr_string[3][7];
+
+	for (int i = 0; i < 3; i++)
+	{
+		relocation_addr_int[i] = strtol(SubString(all_instruction, locations[i] * 2, locations[i] * 2 + 5), NULL, 16) + start_addr;
+
+		sprintf(relocation_addr_string[i], "%X", relocation_addr_int[i]);	// 런타임 에러나는 이유
+		relocation_addr_string[i][6] = '\0';
+
+	}
+
+
+	all_length -= 1;
+
 	for (int i = 0; i < all_length; i++)
 	{
-		int instruction = strtol(SubString(temp_all,0,1),NULL,16); 
-			
 		for (int j = 0; j < location_count; j++)
-		{	
-				
-			if (i - 1 == locations[j])
+		{
+			if (i == locations[j] * 2)
 			{
-				instruction += 9;
-			}
-			else if (i - 2 == locations[j])
-			{
-				instruction -= 44;
-			}
-		}
-		
 
-		fprintf(after_relocation,"%d %02X\n", address, instruction);
-		temp_all = SubString(temp_all, 2, all_length);
-		all_length = strlen(temp_all);
+				for (int k = 0; k < 6; k++)
+				{
+					all_instruction[i + k] = relocation_addr_string[j][k];
+				}
+			}
+
+		}
+	}
+
+	for (int i = 0; i < all_length; i++)
+	{
+		int instruction = strtol(SubString(all_instruction, 0, 1), NULL, 16);
+
+
+		fprintf(after_relocation, "%d %02X\n", address, instruction);
+		all_instruction = SubString(all_instruction, 2, all_length);
+		all_length = strlen(all_instruction);
 		address += 1;
 	}
-	
-	int relocation_addr[3];
-	for(int i=0;i<3;i++)
-		relocation_addr[i] = strtol(SubString(all_instruction, locations[i] * 2, locations[i] * 2 + 5), NULL, 16);
-	
+
 
 	address = start_addr;
 	printf("before modification:\n");
 	printf("Address  instruction\n");
-	for(int i= 0 ;i<3;i++)
-		printf("%d %10X\n", address + locations[i], relocation_addr[i]);
+	for (int i = 0; i < 3; i++)
+		printf("%d %10X\n", address + locations[i], relocation_addr_int[i] - start_addr);
 
 	printf("\n");
 	printf("after modification:\n");
 	printf("Address  instruction\n");
 	for (int i = 0; i < 3; i++)
-		printf("%d %10X\n", address + locations[i], relocation_addr[i] + start_addr);
+		printf("%d %10X\n", address + locations[i], relocation_addr_int[i]);
 
-	
 
-	
+
+
 	free(lines);
 	free(temps);
 	fclose(after_relocation);
